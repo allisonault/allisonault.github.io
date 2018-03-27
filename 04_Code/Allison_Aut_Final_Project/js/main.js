70fbd749a19647ee8ddc6c26b82be10f2a5fee22
allisonault <allison.ault@gmail.com>
2018-03-27 11:06:11 -0600

    Final Project Update
    Code for final project - day before due date.

---
+++ 04_Code/Allison_Aut_Final_Project/js/main.js
@@ -0,0 +1,13 @@
+
+// ******* Home Page JQuery *********
+
+// Fading navigation
+//WHEN the user scrolls the page
+  // if the user has scrolled more than 500px
+    // Header and nav bar become opace
+  // else
+    // Header and nav bar have solid color
+
+
+
+// Animated buttons and callout text
--- Allison_Aut_Final_Project/js/main.js
+++ Allison_Aut_Final_Project/js/main.js
@@ -1,1 +1,13 @@
-// Make it rain!
+
+// ******* Home Page JQuery *********
+
+// Fading navigation
+//WHEN the user scrolls the page
+  // if the user has scrolled more than 500px
+    // Header and nav bar become opace
+  // else
+    // Header and nav bar have solid color
+
+
+
+// Animated buttons and callout text
